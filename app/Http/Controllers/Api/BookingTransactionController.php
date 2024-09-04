6839256468d8e220aa86d3788edfb5ced08a3604
farhanlumoshive <farhan.arfianto@lumoshive.com>
2024-09-04 09:25:46 +0700

    feat: add api book office

--- app/Http/Controllers/Api/BookingTransactionController.php
+++ app/Http/Controllers/Api/BookingTransactionController.php
@@ -3,9 +3,31 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBookingTransactionRequest;
+use App\Http\Resources\Api\BookingTransactionResource;
+use App\Models\BookingTransaction;
+use App\Models\OfficeSpace;
 use Illuminate\Http\Request;
 
 class BookingTransactionController extends Controller
 {
-    //
+    public function store(StoreBookingTransactionRequest $request)
+    {
+        $validateData = $request->validated();
+
+        $officeSpace = OfficeSpace::find($validateData['office_space_id']);
+
+        $validateData['is_paid'] = false;
+        $validateData['booking_trx_id'] = BookingTransaction::generateUniqueTrxId();
+        $validateData['duration'] = $officeSpace->duration;
+        $validateData['ended_at'] = (new \DateTime($validateData['started_at']))->modify("+{$officeSpace->duration} days")->format('Y-m-d');
+
+        $bookingTransaction = BookingTransaction::create($validateData);
+
+        // kirim sms
+
+        // return response
+        $bookingTransaction->load('officeSpace');
+        return new BookingTransactionResource($bookingTransaction);
+    }
 }
